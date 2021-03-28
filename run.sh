9cc930fab39afded5c0cc5aaf03e2ff92b238e5c
Ubuntu <ubuntu@ubuntu2010.linuxvmimages.local>
2021-03-28 16:50:48 -0400

    fixed folder name

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./NGS_data_analysis/main_process_raw_NGS_data.sh ./uASPIre_RBS_example/config_uASPIre_RBS_example.cfg
+./NGS_data_analysis/main_process_raw_NGS_data.sh ./uASPIre_example/config_uASPIre_example.cfg
